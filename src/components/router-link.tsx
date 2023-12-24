4ebfaa15360ca9a57961e0ab6b0bf9a17cf0790b
Tazmeer Ul Hassan <tazmeerulhassan01@gmail.com>
2023-12-24 11:00:07 +0500

    init

---
+++ src/components/router-link.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import type { LinkProps } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+interface RouterLinkProps extends Omit<LinkProps, 'to'> {
+  href: string;
+}
+
+/**
+ * This is an adapted for `react-router-dom/link` component.
+ * We use this to help us maintain consistency between CRA and Next.js
+ */
+export const RouterLink = forwardRef(function RouterLink(props: RouterLinkProps, ref: any) {
+  const { href, ...other } = props;
+
+  return (
+    <Link
+      ref={ref}
+      to={href}
+      {...other}
+    />
+  );
+});
+
+RouterLink.propTypes = {
+  href: PropTypes.string.isRequired
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,67 @@
+import { lazy } from 'react';
+import type { RouteObject } from 'react-router';
+import { Outlet } from 'react-router-dom';
+
+import { Layout as MarketingLayout } from 'src/layouts/marketing';
+
+import { authRoutes } from './auth';
+import { authDemoRoutes } from './auth-demo';
+import { componentsRoutes } from './components';
+import { dashboardRoutes } from './dashboard';
+
+const Error401Page = lazy(() => import('src/pages/401'));
+const Error404Page = lazy(() => import('src/pages/404'));
+const Error500Page = lazy(() => import('src/pages/500'));
+
+const HomePage = lazy(() => import('src/pages/index'));
+const ContactPage = lazy(() => import('src/pages/contact'));
+const CheckoutPage = lazy(() => import('src/pages/checkout'));
+const PricingPage = lazy(() => import('src/pages/pricing'));
+
+export const routes: RouteObject[] = [
+  {
+    element: (
+      <MarketingLayout>
+        <Outlet />
+      </MarketingLayout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'pricing',
+        element: <PricingPage />
+      },
+      ...componentsRoutes
+    ]
+  },
+  ...authRoutes,
+  ...authDemoRoutes,
+  ...dashboardRoutes,
+  {
+    path: 'checkout',
+    element: <CheckoutPage />
+  },
+  {
+    path: 'contact',
+    element: <ContactPage />
+  },
+  {
+    path: '401',
+    element: <Error401Page />
+  },
+  {
+    path: '404',
+    element: <Error404Page />
+  },
+  {
+    path: '500',
+    element: <Error500Page />
+  },
+  {
+    path: '*',
+    element: <Error404Page />
+  }
+];
